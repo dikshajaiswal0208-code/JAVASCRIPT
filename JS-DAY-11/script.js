30771bb7a88e55fb5d8893da8280d4e1e8662a90
dikshajaiswal0208-code <dikshajaiswal02.08@gmail.com>
2026-02-09 13:37:01 +0530

    JS

---
+++ JS-DAY-11/script.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click",()=>{
+            //create h2 tag
+    const subHead = document.createElement("h2").textContent="New Sub Heading"
+            //create image
+    const image=document.createElement("img");
+    image.setAttribute("src","https://krmangalam.edu.in/_next/image?url=%2Fkrmuniversity.webp&w=1200&q=75")
+
+    image.setAttribute("alt","K.R Mangalam");
+
+          //append both h2 and image inside content div
+    document.getElementById("content").append(subHead)
+    document.getElementById("content").append(image)
+})
